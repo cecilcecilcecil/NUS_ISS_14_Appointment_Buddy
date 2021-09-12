14a49380b7e2b6e6216ab4c7510047ff3976d97f
lowyi88 <65527770+lowyi88@users.noreply.github.com>
2021-09-12 11:48:16 +0800

    Add project files.

---
+++ HSPService/DBContext/ServiceContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HSPService.Models;
+
+
+namespace HSPService.DBContext
+{
+    public class ServiceContext : DbContext
+    {
+        public ServiceContext(DbContextOptions<ServiceContext> options) : base(options)
+        {
+        }
+        public DbSet<Service> Service { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Service>().HasData(
+                new Service
+                {
+                    Id = 1,
+                    Services = "testServices",
+                    CreatedBy = "S1234567C",
+                    CreatedDate = System.DateTime.Now,
+                    LastUpdatedBy = "S234567C",
+                    LastUpdatedDate = System.DateTime.Now,
+                    IsDeleted  = false
+                }
+            );
+        }
+    }
+}
---
+++ HSPService/Models/Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSPService.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Services { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public DateTime LastUpdatedDate { get; set; }
+        public Boolean IsDeleted { get; set; }
+    }
+}
+
