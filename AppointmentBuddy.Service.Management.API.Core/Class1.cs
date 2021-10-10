5306424dd7c1e926873f27e6228d7ce7f6e153b2
JUN WEI SEE <see.see@ncs.com.sg>
2021-10-10 19:23:37 +0800

    Management API test

---
+++ AppointmentBuddy.Service.Management.API.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AppointmentBuddy.Service.Management.API.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ AppointmentBuddy.Service.Management.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppointmentBuddy.Service.Management.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
