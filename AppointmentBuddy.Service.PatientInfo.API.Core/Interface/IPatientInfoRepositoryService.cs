adb70d8e25f5cab07671ec9721bc730f5e6f296a
Liran Wang <e0385291@u.nus.edu>
2021-10-16 01:08:57 +0800

    Add PatientInfo API

---
+++ AppointmentBuddy.Service.PatientInfo.API.Core/Interface/IPatientInfoRepositoryService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppointmentBuddy.Service.PatientInfo.API.Core.Interface
+{
+    class IPatientInfoRepositoryService
+    {
+    }
+}
