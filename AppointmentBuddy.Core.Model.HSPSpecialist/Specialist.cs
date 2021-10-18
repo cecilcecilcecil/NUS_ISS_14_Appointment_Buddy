3a5a368673eb8cd9e46ae14c15315dec50c33956
lowyi88 <65527770+lowyi88@users.noreply.github.com>
2021-10-19 00:03:19 +0800

    Add in Specialist maintenance page
    Add in search, delete function

---
+++ AppointmentBuddy.Core.Model.HSPSpecialist/Specialist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentBuddy.Core.Model
+{
+    public class Specialist
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string NRIC { get; set; }
+        public string ServiceDescription { get; set; }
+        [Required]
+        public string Contact { get; set; }
+        [Required]
+        public bool? Available { get; set; }
+        [Required]
+        public String Address { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public String Email { get; set; }
+        public String CreatedBy { get; set; } = "System";
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public String LastUpdatedBy { get; set; } = "System";
+        public DateTime LastUpdatedDate { get; set; } = DateTime.Now;
+        [Required]
+        public bool? IsDeleted { get; set; } = false;
+    }
+}
