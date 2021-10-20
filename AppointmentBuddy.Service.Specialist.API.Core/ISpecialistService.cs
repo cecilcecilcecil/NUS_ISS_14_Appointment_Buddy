9a688e96f938d5545aa88b21523d5fd4247573e8
JUN WEI SEE <see.see@ncs.com.sg>
2021-10-20 12:23:10 +0800

    API Docker files and Specialist API

---
+++ AppointmentBuddy.Service.Specialist.API.Core/ISpecialistService.cs
@@ -0,0 +1,10 @@
+﻿using M = AppointmentBuddy.Core.Model;
+using System.Threading.Tasks;
+
+namespace AppointmentBuddy.Service.Specialist.API.Core.Interface
+{
+    public interface ISpecialistService
+    {
+
+    }
+}
---
+++ AppointmentBuddy.Service.Specialist.API/Controllers/SpecialistController.cs
@@ -0,0 +1,39 @@
+﻿using AppointmentBuddy.Core.Common.Helper;
+using AppointmentBuddy.Service.Specialist.API.Core.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using M = AppointmentBuddy.Core.Model;
+
+namespace AppointmentBuddy.Service.Specialist.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("v{v:apiVersion}/api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SpecialistController : Controller
+    {
+        private readonly ISpecialistService _specialistService;
+        private readonly ILogger<SpecialistController> _logger;
+
+        public SpecialistController(ISpecialistService specialistService, ILogger<SpecialistController> logger)
+        {
+            _logger = logger;
+            _specialistService = specialistService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("health")]
+        public IActionResult Health()
+        {
+            return Ok(DateTime.Now.ToString());
+        }
+    }
+}
