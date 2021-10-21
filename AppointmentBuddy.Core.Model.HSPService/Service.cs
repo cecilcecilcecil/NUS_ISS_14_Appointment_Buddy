6af2a2e542431137717cba8f0c8d62d71cec8d3f
lowyi88 <65527770+lowyi88@users.noreply.github.com>
2021-10-21 12:15:14 +0800

    services view
    services view

---
+++ AppointmentBuddy.Core.Model.HSPService/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppointmentBuddy.Core.Model
+{
+    public class Services
+    {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string CreatedBy { get; set; } = "System";
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string LastUpdatedBy { get; set; } = "System";
+        public DateTime LastUpdatedDate { get; set; } = DateTime.Now;
+        public bool? IsDeleted { get; set; } = false;
+    }
+}
---
+++ NUS_ISS_14_Appointment_Buddy/Controllers/Service.cs
@@ -0,0 +1,50 @@
+﻿using AppointmentBuddy.Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUS_ISS_14_Appointment_Buddy.Controllers
+{
+    public class Service : Controller
+    {
+        public async Task<IActionResult> IndexAsync(int id)
+        {
+            List<AppointmentBuddy.Core.Model.Services> ServiceList = new List<AppointmentBuddy.Core.Model.Services>();
+
+            if (id == 0)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync("http://localhost:63742/api/Services"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        ServiceList = JsonConvert.DeserializeObject<List<AppointmentBuddy.Core.Model.Services>>(apiResponse);
+                    }
+                }
+                return View("Service", ServiceList);
+            }
+            else
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync("http://localhost:63742/api/Services/" + id))
+                    {
+                        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        {
+                            string apiResponse = await response.Content.ReadAsStringAsync();
+
+                            var apiResponseArray = "[" + apiResponse + "]";
+                            ServiceList = JsonConvert.DeserializeObject<List<AppointmentBuddy.Core.Model.Services>>(apiResponseArray);
+                        }
+                    }
+                }
+                return View("Service", ServiceList);
+            }
+        }
+    }
+}
